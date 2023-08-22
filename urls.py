4d26e741679d5904699f8f6e33b13704d2ac91ea
rajesheswaran <142887071+rajesheswaran@users.noreply.github.com>
2023-08-22 15:05:33 +0530

    final commit

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from . import views 
 
 urlpatterns = [
-    path('admin/,admin.site.urls'),
-    path('',include('peoples.urls'))
+    path('',views.home,name='home'),
+    path('competition_list',views.competition_list,name='competion-list'),
 ]
